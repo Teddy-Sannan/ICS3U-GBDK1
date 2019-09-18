3346aa93fb6350b47ff8f4468c44fff331d05b7b
Teddy-Sannan <54954634+Teddy-Sannan@users.noreply.github.com>
2019-09-18 13:22:57 -0400

    Add files via upload

---
+++ game.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Teddy Sannan
+// Created on: Spetember 2019
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main() {
+    printf("Hello, World!");
+}
